a4fdfee93a1dc58b4fbf54e0f75d8b0cebd8c11a
Robert Lyn <lenny4422@gmail.com>
2022-08-30 16:21:32 -0400

    Use new namespace syntax

--- Gamba.Tests/Games/RouletteStatsTests.cs
+++ Gamba.Tests/Games/RouletteStatsTests.cs
@@ -5,63 +5,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gamba.Tests.Games
+namespace Gamba.Tests.Games;
+
+internal class RouletteStatsTests
 {
-    internal class RouletteStatsTests
+    private RouletteStats _stats;
+    private const int _startingWallet = 1000;
+
+    [SetUp]
+    public void Setup()
     {
-        private RouletteStats _stats;
-        private const int _startingWallet = 1000;
+        _stats = new RouletteStats(_startingWallet);
+    }
 
-        [SetUp]
-        public void Setup()
-        {
-            _stats = new RouletteStats(_startingWallet);
-        }
+    [Test(ExpectedResult = _startingWallet)]
+    public int Wallet_StartingAmount()
+    {
+        return _stats.Wallet;
+    }
 
-        [Test(ExpectedResult = _startingWallet)]
-        public int Wallet_StartingAmount()
-        {
-            return _stats.Wallet;
-        }
+    [Test(ExpectedResult = _startingWallet)]
+    public int MinWallet_StartingAmount()
+    {
+        return _stats.MinWallet;
+    }
 
-        [Test(ExpectedResult = _startingWallet)]
-        public int MinWallet_StartingAmount()
-        {
-            return _stats.MinWallet;
-        }
+    [Test(ExpectedResult = _startingWallet)]
+    public int MaxWallet_StartingAmount()
+    {
+        return _stats.MaxWallet;
+    }
 
-        [Test(ExpectedResult = _startingWallet)]
-        public int MaxWallet_StartingAmount()
-        {
-            return _stats.MaxWallet;
-        }
+    [TestCase(20, ExpectedResult = _startingWallet - 20)]
+    public int AddBet_Loss_DecreaseWallet(int betAmount)
+    {
+        _stats.AddBet(false, Draw.One, betAmount, 2);
 
-        [TestCase(20, ExpectedResult = _startingWallet - 20)]
-        public int AddBet_Loss_DecreaseWallet(int betAmount)
-        {
-            _stats.AddBet(false, Draw.One, betAmount, 2);
+        return _stats.Wallet;
+    }
 
-            return _stats.Wallet;
-        }
+    [TestCase(20, 3, ExpectedResult = _startingWallet - 20 + (20 * 3))]
+    public int AddBet_Win_IncreaseWallet(int betAmount, int winReturn)
+    {
+        _stats.AddBet(true, Draw.One, betAmount, winReturn);
 
-        [TestCase(20, 3, ExpectedResult = _startingWallet - 20 + (20 * 3))]
-        public int AddBet_Win_IncreaseWallet(int betAmount, int winReturn)
-        {
-            _stats.AddBet(true, Draw.One, betAmount, winReturn);
+        return _stats.Wallet;
+    }
 
-            return _stats.Wallet;
-        }
+    [Test]
+    public void AddBet_ThreeDraws_IncreaseTotals()
+    {
+        _stats.AddBet(true, Draw.One, 20, 1);
+        _stats.AddBet(true, Draw.One, 20, 1);
+        _stats.AddBet(false, Draw.One, 20, 1);
 
-        [Test]
-        public void AddBet_ThreeDraws_IncreaseTotals()
-        {
-            _stats.AddBet(true, Draw.One, 20, 1);
-            _stats.AddBet(true, Draw.One, 20, 1);
-            _stats.AddBet(false, Draw.One, 20, 1);
-
-            Assert.That(_stats.TotalWins, Is.EqualTo(2));
-            Assert.That(_stats.TotalLosses, Is.EqualTo(1));
-            Assert.That(_stats.TotalDraws, Is.EqualTo(3));
-        }
+        Assert.That(_stats.TotalWins, Is.EqualTo(2));
+        Assert.That(_stats.TotalLosses, Is.EqualTo(1));
+        Assert.That(_stats.TotalDraws, Is.EqualTo(3));
     }
 }
--- Gamba.Tests/Games/RouletteWheelTests.cs
+++ Gamba.Tests/Games/RouletteWheelTests.cs
@@ -6,88 +6,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gamba.Tests.Games
+namespace Gamba.Tests.Games;
+
+internal class RouletteWheelTests
 {
-    internal class RouletteWheelTests
+    private RouletteWheel _roulette;
+    private Mock<Random> _randomMock;
+
+    [SetUp]
+    public void Setup()
     {
-        private RouletteWheel _roulette;
-        private Mock<Random> _randomMock;
+        _randomMock = new Mock<Random>();
+        _roulette = new RouletteWheel(_randomMock.Object);
+    }
 
-        [SetUp]
-        public void Setup()
-        {
-            _randomMock = new Mock<Random>();
-            _roulette = new RouletteWheel(_randomMock.Object);
-        }
+    [Test]
+    public void GetValidBets_AllValidBetsByOdds()
+    {
+        var actual = _roulette.GetValidBets();
 
-        [Test]
-        public void GetValidBets_AllValidBetsByOdds()
-        {
-            var actual = _roulette.GetValidBets();
+        var expected = new int[] { 1, 3, 5, 10, 20 };
 
-            var expected = new int[] { 1, 3, 5, 10, 20 };
+        CollectionAssert.AreEqual(expected, actual);
+    }
 
-            CollectionAssert.AreEqual(expected, actual);
-        }
+    [TestCase(1, ExpectedResult = Draw.One)]
+    [TestCase(2, ExpectedResult = Draw.One)]
+    [TestCase(3, ExpectedResult = Draw.One)]
+    [TestCase(4, ExpectedResult = Draw.One)]
+    [TestCase(5, ExpectedResult = Draw.One)]
+    [TestCase(6, ExpectedResult = Draw.One)]
+    [TestCase(7, ExpectedResult = Draw.One)]
+    [TestCase(8, ExpectedResult = Draw.One)]
+    [TestCase(9, ExpectedResult = Draw.One)]
+    [TestCase(10, ExpectedResult = Draw.One)]
+    [TestCase(11, ExpectedResult = Draw.One)]
+    [TestCase(12, ExpectedResult = Draw.One)]
+    [TestCase(13, ExpectedResult = Draw.Three)]
+    [TestCase(14, ExpectedResult = Draw.Three)]
+    [TestCase(15, ExpectedResult = Draw.Three)]
+    [TestCase(16, ExpectedResult = Draw.Three)]
+    [TestCase(17, ExpectedResult = Draw.Three)]
+    [TestCase(18, ExpectedResult = Draw.Three)]
+    [TestCase(19, ExpectedResult = Draw.Five)]
+    [TestCase(20, ExpectedResult = Draw.Five)]
+    [TestCase(21, ExpectedResult = Draw.Five)]
+    [TestCase(22, ExpectedResult = Draw.Five)]
+    [TestCase(23, ExpectedResult = Draw.Ten)]
+    [TestCase(24, ExpectedResult = Draw.Ten)]
+    [TestCase(25, ExpectedResult = Draw.Twenty)]
+    public Draw GetNextDraw_Random_ValidBet(int randomValue)
+    {
+        _randomMock.Setup(r => r.Next(It.IsAny<int>(), It.IsAny<int>())).Returns(randomValue);
 
-        [TestCase(1, ExpectedResult = Draw.One)]
-        [TestCase(2, ExpectedResult = Draw.One)]
-        [TestCase(3, ExpectedResult = Draw.One)]
-        [TestCase(4, ExpectedResult = Draw.One)]
-        [TestCase(5, ExpectedResult = Draw.One)]
-        [TestCase(6, ExpectedResult = Draw.One)]
-        [TestCase(7, ExpectedResult = Draw.One)]
-        [TestCase(8, ExpectedResult = Draw.One)]
-        [TestCase(9, ExpectedResult = Draw.One)]
-        [TestCase(10, ExpectedResult = Draw.One)]
-        [TestCase(11, ExpectedResult = Draw.One)]
-        [TestCase(12, ExpectedResult = Draw.One)]
-        [TestCase(13, ExpectedResult = Draw.Three)]
-        [TestCase(14, ExpectedResult = Draw.Three)]
-        [TestCase(15, ExpectedResult = Draw.Three)]
-        [TestCase(16, ExpectedResult = Draw.Three)]
-        [TestCase(17, ExpectedResult = Draw.Three)]
-        [TestCase(18, ExpectedResult = Draw.Three)]
-        [TestCase(19, ExpectedResult = Draw.Five)]
-        [TestCase(20, ExpectedResult = Draw.Five)]
-        [TestCase(21, ExpectedResult = Draw.Five)]
-        [TestCase(22, ExpectedResult = Draw.Five)]
-        [TestCase(23, ExpectedResult = Draw.Ten)]
-        [TestCase(24, ExpectedResult = Draw.Ten)]
-        [TestCase(25, ExpectedResult = Draw.Twenty)]
-        public Draw GetNextDraw_Random_ValidBet(int randomValue)
-        {
-            _randomMock.Setup(r => r.Next(It.IsAny<int>(), It.IsAny<int>())).Returns(randomValue);
+        var result = _roulette.GetNextDraw();
 
-            var result = _roulette.GetNextDraw();
+        return result;
+    }
 
-            return result;
-        }
+    [Test]
+    public void GetNextDraw_CallsRandom()
+    {
+        var result = _roulette.GetNextDraw();
 
-        [Test]
-        public void GetNextDraw_CallsRandom()
-        {
-            var result = _roulette.GetNextDraw();
+        _randomMock.Verify(r => r.Next(1, 26));
+    }
 
-            _randomMock.Verify(r => r.Next(1, 26));
-        }
+    [Test]
+    public void GetWinReturn_InvalidDraw_ThrowsException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => _roulette.GetWinReturn(0));
+    }
 
-        [Test]
-        public void GetWinReturn_InvalidDraw_ThrowsException()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() => _roulette.GetWinReturn(0));
-        }
+    [TestCase(Draw.One, ExpectedResult = 2)]
+    [TestCase(Draw.Three, ExpectedResult = 4)]
+    [TestCase(Draw.Five, ExpectedResult = 6)]
+    [TestCase(Draw.Ten, ExpectedResult = 11)]
+    [TestCase(Draw.Twenty, ExpectedResult = 21)]
+    public int GetWinReturn_Draw_ReturnsResult(Draw draw)
+    {
+        var result = _roulette.GetWinReturn(draw);
 
-        [TestCase(Draw.One, ExpectedResult = 2)]
-        [TestCase(Draw.Three, ExpectedResult = 4)]
-        [TestCase(Draw.Five, ExpectedResult = 6)]
-        [TestCase(Draw.Ten, ExpectedResult = 11)]
-        [TestCase(Draw.Twenty, ExpectedResult = 21)]
-        public int GetWinReturn_Draw_ReturnsResult(Draw draw)
-        {
-            var result = _roulette.GetWinReturn(draw);
-
-            return result;
-        }
+        return result;
     }
 }
